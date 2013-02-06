1a4c063441e4a8dc5d77c6b39dee9edd3da7a5c8
Chad Schulz <chad.awesome@gmail.com>
2013-02-05 23:55:55 -0600

    Testing and updates to bandSearch() and albumInfo()

--- js/bandcamp.js
+++ js/bandcamp.js
@@ -10,17 +10,18 @@ var Bandcamp = WinJS.Class.define(
     this.debug = false;
 
     // Bandcamp API key
-    this.key = key;
+    this._key = key;
+
     this.module = 'band';
     this.version = 3;
-    this.url = 'http://api.bandcamp.com/api/album/2/';
+    this._baseUrl = 'http://api.bandcamp.com/api/';
 
-    this.moduleBand = 'band';
-    this.moduleBandVersion = 3;
+    this._moduleBand = 'band';
+    this._moduleBandVersion = 3;
     // search, discography, info
 
-    this.moduleAlbum = 'album';
-    this.moduleAlbumVersion = 2;
+    this._moduleAlbum = 'album';
+    this._moduleAlbumVersion = 2;
     // info
 
     this.moduleTrack = 'track';
@@ -31,15 +32,29 @@ var Bandcamp = WinJS.Class.define(
     this.moduleUrlVersion = 1;
     // info
   },
+
   // Instance Members
   {
-    bandSearch: function (name) {
+
+    /*
+     * bandSearch() Search for a band name
+     * @param name <String> The band name term to search on
+     * @sample http://api.bandcamp.com/api/band/3/search?key=<key>&name=baron%20von%20luxxury
+     */
+    bandSearch: function(name) {
+
+      var searchTerm;
       // TODO: account for All URL-escaping
-      var searchTerm = term.replace(' ', '%20');
+      searchTerm = name.replace(' ', '%20');
+      searchTerm = name.replace('#', '%23');
+      searchTerm = name.replace('&', '%26');
+      searchTerm = name.replace('?', '%3F');
+
+      // Return the xhr object
       return WinJS.xhr({
         responseType: 'json',
         type: 'GET',
-        url: this.url + 'search?key=' + this.key + '&name=' + searchTerm
+        url: this._baseUrl + this._moduleBand + '/' + this._moduleBandVersion + '/search?key=' + this._key + '&name=' + searchTerm
       });
     },
 
@@ -59,11 +74,18 @@ var Bandcamp = WinJS.Class.define(
       });
     },
 
-    albumInfo: function (albumId) {
+    /*
+     * albumInfo() Retrieve the album information based on album ID
+     * @param name <String> The ID representing the album
+     * @sample http://api.bandcamp.com/api/album/2/info?key=<key>&album_id=2587417518
+     */
+    albumInfo: function(albumId) {
+
+      // Return the xhr object
       return WinJS.xhr({
         responseType: 'json',
         type: 'GET',
-        url: this.url + 'info?key=' + this.key + '&album_id=' + albumId
+        url: this._baseUrl + this._moduleAlbum + '/' + this._moduleAlbumVersion + '/info?key=' + this._key + '&album_id=' + albumId
       });
     },
 
