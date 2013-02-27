eb0db13987c93249f21eac0bd3c10555ef0fa832
Chad Schulz <chad.awesome@gmail.com>
2013-02-26 22:20:39 -0600

    Add and test bandDiscography

--- js/bandcamp.js
+++ js/bandcamp.js
@@ -1,137 +1,119 @@
 ﻿// Bandcamp API
 // @author Chad Schulz
+// @version 0.1.0
 
 var Bandcamp = WinJS.Class.define(
 
   // Constructor
   function (key) {
 
-    // Debug mode -- '&debug'
-    this.debug = false;
+      // Debug mode -- '&debug'
+      this.debug = false;
 
-    // Bandcamp API key
-    this._key = key;
+      // Bandcamp API key
+      this._key = key;
 
-    this.module = 'band';
-    this.version = 3;
-    this._baseUrl = 'http://api.bandcamp.com/api/';
+      // Version
+      this.VERSION = '0.1.0';
 
-    this._moduleBand = 'band';
-    this._moduleBandVersion = 3;
-    // search, discography, info
+      this.BASE_URL = 'http://api.bandcamp.com/api/';
 
-    this._moduleAlbum = 'album';
-    this._moduleAlbumVersion = 2;
-    // info
+      // Default module
+      this.moduleDefault = 'band';
+      this.versionDefault = 3;
 
-    this._moduleTrack = 'track';
-    this._moduleTrackVersion = 3;
-    // info
+      // Band module [search. discography, info]
+      this.MODULE_BAND = 'band';
+      this.MODULE_BAND_VERSION = 3;
 
-    this._moduleUrl = 'url';
-    this._moduleUrlVersion = 1;
-    // info
+      // Album module [info]
+      this.MODULE_ALBUM = 'album';
+      this.MODULE_ALBUM_VERSION = 2;
+
+      // Track module [info]
+      this.moduleTrack = 'track';
+      this.moduleTrackVersion = 3;
+
+      // Url module [info]
+      this.moduleUrl = 'url';
+      this.moduleUrlVersion = 1;
   },
 
   // Instance Members
   {
 
-    /*
-     * bandSearch() Search for a band name
-     * @param name <String> The band name term to search on
-     * @sample http://api.bandcamp.com/api/band/3/search?key=<key>&name=baron%20von%20luxxury
-     */
-    bandSearch: function(name) {
+      /*
+       * bandSearch() Search for a band name
+       * @param name <String> The band name term to search on
+       * @sample http://api.bandcamp.com/api/band/3/search?key=<key>&name=baron%20von%20luxxury
+       */
+      bandSearch: function (name) {
 
-      var searchTerm;
-      // TODO: account for All URL-escaping
-      searchTerm = name.replace(' ', '%20');
-      searchTerm = name.replace('#', '%23');
-      searchTerm = name.replace('&', '%26');
-      searchTerm = name.replace('?', '%3F');
+          var searchTerm;
+          // TODO: account for All URL-escaping
+          searchTerm = name.replace(' ', '%20');
+          searchTerm = name.replace('#', '%23');
+          searchTerm = name.replace('&', '%26');
+          searchTerm = name.replace('?', '%3F');
 
-      // Return the xhr object
-      return WinJS.xhr({
-        responseType: 'json',
-        type: 'GET',
-        url: this._baseUrl + this._moduleBand + '/' + this._moduleBandVersion + '/search?key=' + this._key + '&name=' + searchTerm
-      });
-    },
+          // Return the xhr object
+          return WinJS.xhr({
+              responseType: 'json',
+              type: 'GET',
+              url: this.BASE_URL + this.MODULE_BAND + '/' + this.MODULE_BAND_VERSION + '/search?key=' + this._key + '&name=' + searchTerm
+          });
+      },
 
-    /*
-     * bandDiscogrophy() Retrieve a band discogrophy given a band ID
-     * @param name <String> The ID representing the album
-     * @sample
-     */
-    bandDiscogrophy: function (bandId) {
+      /*
+       * bandDiscography() Search for a band's discography
+       * @param bandId <Number> The band's id of which to search
+       */
+      bandDiscography: function (bandId) {
+          return WinJS.xhr({
+              responseType: 'json',
+              type: 'GET',
+              url: this.BASE_URL + this.MODULE_BAND + '/' + this.MODULE_BAND_VERSION + '/discography?key=' + this._key + '&band_id=' + bandId
+          });
+      },
 
-      // Return the xhr object
-      return WinJS.xhr({
-        responseType: 'json',
-        type: 'GET',
-        url: this._baseUrl + this._moduleBand + '/' + this._moduleBandVersion + '/discography?key=' + this._key + '&band_id=' + bandId
-      });
-    },
+      bandInfo: function (bandId) {
+          return WinJS.xhr({
+              responseType: 'json',
+              type: 'GET',
+              url: this.url + 'info?key=' + this.key + '&band_id=' + bandId
+          });
+      },
 
-    /*
-     * bandInfo() Retrieve band info given a band ID
-     * @param name <String> The ID representing the album
-     * @sample
-     */
-    bandInfo: function (bandId) {
+      /*
+       * albumInfo() Retrieve the album information based on album ID
+       * @param name <String> The ID representing the album
+       * @sample http://api.bandcamp.com/api/album/2/info?key=<key>&album_id=2587417518
+       */
+      albumInfo: function (albumId) {
 
-      // Return the xhr object
-      return WinJS.xhr({
-        responseType: 'json',
-        type: 'GET',
-        url: this._baseUrl + this._moduleBand + '/' + this._moduleBandVersion + '/info?key=' + this._key + '&band_id=' + bandId
-      });
-    },
+          // Return the xhr object
+          return WinJS.xhr({
+              responseType: 'json',
+              type: 'GET',
+              url: this.BASE_URL + this.MODULE_ALBUM + '/' + this.MODULE_ALBUM_VERSION + '/info?key=' + this._key + '&album_id=' + albumId
+          });
+      },
 
-    /*
-     * albumInfo() Retrieve the album information based on album ID
-     * @param albumId <String> The album ID
-     * @sample http://api.bandcamp.com/api/album/2/info?key=<key>&album_id=2587417518
-     */
-    albumInfo: function(albumId) {
+      trackInfo: function (trackId) {
+          return WinJS.xhr({
+              responseType: 'json',
+              type: 'GET',
+              url: this.url + 'info?key=' + this.key + '&track_id=' + trackId
+          });
+      },
 
-      // Return the xhr object
-      return WinJS.xhr({
-        responseType: 'json',
-        type: 'GET',
-        url: this._baseUrl + this._moduleAlbum + '/' + this._moduleAlbumVersion + '/info?key=' + this._key + '&album_id=' + albumId
-      });
-    },
-
-    /*
-     * trackInfo() Retrieve the track information based on track ID
-     * @param albumId <String> The track ID
-     * @sample
-     */
-    trackInfo: function (trackId) {
-
-      // Return the xhr object
-      return WinJS.xhr({
-        responseType: 'json',
-        type: 'GET',
-        url: this._baseUrl + this._moduleTrack + '/' + this._moduleTrackVersion + '/info?key=' + this._key + '&track_id=' + trackId
-      });
-    },
-
-    /*
-     * urlInfo() Retrieve the url information based on a URL
-     * @param url <String> The URL
-     * @sample
-     */
-    urlInfo: function (url) {
-
-      // Return the xhr object
-      return WinJS.xhr({
-        responseType: 'json',
-        type: 'GET',
-        url: this._baseUrl + this._moduleUrl + '/' + this._moduleUrlVersion + '/info?key=' + this._key + '&url=' + url
-      });
-    }
+      urlInfo: function (url) {
+          return WinJS.xhr({
+              responseType: 'json',
+              type: 'GET',
+              url: this.url + 'info?key=' + this.key + '&url=' + url
+          });
+      }
   },
   // Static Members
   {
